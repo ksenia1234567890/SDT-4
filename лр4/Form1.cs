6ebab2e6489b4383b9f3b482f68c89df72416143
ksenia 1234567890 <eurosetv3.04.05.14@gmail.com>
2023-04-04 18:51:50 +0300

    Добавьте файлы проекта.

---
+++ лр4/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace лр4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        public void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            int x = e.X;
+            int y = e.Y;
+            if (x < 340)
+            {
+                Button b = new Button();
+                b.Location = new Point(x, y);
+                b.Size = new Size(128, 63);
+                b.Text = "Кнопка";
+                b.Parent = this;
+            }
+            if (x >= 340)
+            {
+                TextBox tb = new TextBox();
+                tb.Location = new Point(x, y);
+                tb.Size = new Size(128, 63);
+                tb.Text = "Текст";
+                tb.Parent = this;
+            }
+        }
+
+        
+    }
+}
